5ae96210279942fa53ddcfaa317c64fbbbb5bb02
marcelnoehre <marcelnoehre@web.de>
2024-10-29 22:55:01 +0100

    setup app layout with light/dark mode toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectorRef, Component, Inject, inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'sQeeZ-scripting-language.github.io';
+  public mobileQuery!: MediaQueryList;
+  public currentTheme: string = 'dark';
+    
+  private _mobileQueryListener: () => void;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    this.currentTheme = this.getSystemTheme();
+    this.setTheme();
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  toggleTheme(): void {
+    this.currentTheme = this.currentTheme === 'dark' ? 'light' : 'dark';
+    this.setTheme();
+  }
+
+  private getSystemTheme(): string {
+    return (typeof window !== 'undefined' && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'dark' : 'light';
+  }
+
+  private setTheme(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      document.body.classList.toggle('dark-theme', this.currentTheme === 'dark');
+      document.body.classList.toggle('light-theme', this.currentTheme === 'light');
+    }
+  }
 }
